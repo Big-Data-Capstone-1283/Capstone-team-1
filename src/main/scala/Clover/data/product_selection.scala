43ccb388aad6cacdfc45449defc67a2640de7404
Jaceguai De Magalhaes <jaceguai@gmail.com>
2022-04-21 19:51:13 -0400

    Create rules for product selection
    Created a method to define product selection rules according to the date and company.
    Updated DataGenerator with the methods.
    Updated products.csv removing commas messing the file. Most of the commas where in the drugs section.

--- src/main/scala/Clover/data/product_selection.scala
+++ src/main/scala/Clover/data/product_selection.scala
@@ -6,8 +6,8 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import scala.util.Random
 
 //class product_selection(spark: SparkSession){
-//Object to test method productSelection
-//the method will go DataGenerator
+//Object to test method productSelection and productRules
+//the methods will go DataGenerator
 
 object product_selection extends App {
 
@@ -37,52 +37,68 @@ object product_selection extends App {
   val a_products = products.collect()
   val sweepstakesGen = new SweepstakesGen()
   //companies.show()
-  case class SelectedProduct(var product_id: String, var product_category: String, var product_name: String, var product_value: String)
-  var selectedProduct = new SelectedProduct("","","", "")
+  case class Product(var id: Int, var category: String, var name: String, var value: Double)
+  var product = new Product(0,"","", 0D)
+
+ case class ProductParameters(var notSell: String, var prefCategory: String, var oddPrefCategory: Double)
+ var productParameters = new ProductParameters("","", 1D)
 
   for(i<- 1 to 10) {
-    productSelection("","", .50)
-    println(selectedProduct.product_id, selectedProduct.product_category, selectedProduct.product_name, selectedProduct.product_value)
+    productRules()
+    productSelection(productParameters.notSell, productParameters.prefCategory, productParameters.oddPrefCategory)
+    println(product.id, product.category, product.name, product.value)
 
   }
 
 
-  def productSelection(notSell: String, prefCategory: String = "", oddPrefCategory: Double = 1): Unit={
+  def productSelection(notSell: String = "", prefCategory: String = "", oddPrefCategory: Double = 1): Unit= {
     //returns product_id, product_category, product_name, product_price, product_value Double
 
-    if(prefCategory.isEmpty){
+    if (prefCategory.isEmpty) {
       do {
         val result = (a_products(Random.nextInt(productsRows)))
-        selectedProduct.product_id = result(0).toString
-        selectedProduct.product_category = result(1).toString
-        selectedProduct.product_name = result(2).toString
-        selectedProduct.product_value = result(3).toString
+        product.id = result(0).toString.toInt
+        product.category = result(1).toString
+        product.name = result(2).toString
+        product.value = result(3).toString.toDouble
         //println("1 = " + selectedProduct.product_category)
-      }while(notSell.toLowerCase.contains(selectedProduct.product_category.toLowerCase))
-    }else if(sweepstakesGen.shuffle(oddPrefCategory)){
+      } while (notSell.toLowerCase.contains(product.category.toLowerCase))
+    } else if (sweepstakesGen.shuffle(oddPrefCategory)) {
       do {
         //val row = Random.nextInt(productsRows)
         val result = (a_products(Random.nextInt(productsRows)))
-        selectedProduct.product_id = result(0).toString
-        selectedProduct.product_category = result(1).toString
-        selectedProduct.product_name = result(2).toString
-        selectedProduct.product_value = result(3).toString
+        product.id = result(0).toString.toInt
+        product.category = result(1).toString
+        product.name = result(2).toString
+        product.value = result(3).toString.toDouble
         //println("2 = " + selectedProduct.product_category)
-      }while(notSell.toLowerCase.contains(selectedProduct.product_category.toLowerCase) | !prefCategory.toLowerCase.contains(selectedProduct.product_category.toLowerCase))
-    } else{
+      } while (notSell.toLowerCase.contains(product.category.toLowerCase) | !prefCategory.toLowerCase.contains(product.category.toLowerCase))
+    } else {
       do {
         //val row = Random.nextInt(productsRows)
         val result = (a_products(Random.nextInt(productsRows)))
-        selectedProduct.product_id = result(0).toString
-        selectedProduct.product_category = result(1).toString
-        selectedProduct.product_name = result(2).toString
-        selectedProduct.product_value = result(3).toString
+        product.id = result(0).toString.toInt
+        product.category = result(1).toString
+        product.name = result(2).toString
+        product.value = result(3).toString.toDouble
         //println("3 = " + selectedProduct.product_category)
-      }while(notSell.toLowerCase.contains(selectedProduct.product_category.toLowerCase) | prefCategory.toLowerCase.contains(selectedProduct.product_category.toLowerCase) )
+      } while (notSell.toLowerCase.contains(product.category.toLowerCase) | prefCategory.toLowerCase.contains(product.category.toLowerCase))
     }
-
-
   }
 
+    def productRules(date: Int = 1, company: String = ""): Unit = {
+      //according to the date
+      var date = 1 //for testing purposes
+      if (date > 0 & date < 2) {
+        //if(company)
+        {
+          productParameters.notSell = "Car"
+          productParameters.prefCategory = "Movies"
+          productParameters.oddPrefCategory = 1D
 
+        }
+      }
+    }
+
+//end of class
 }
