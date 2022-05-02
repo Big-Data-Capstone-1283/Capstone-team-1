17156c766be617d6a0027512dceba9326e72362d
Caleb Reid <60555714+calebreid2829@users.noreply.github.com>
2022-05-02 10:09:32 -0400

    Update Producer.scala

--- src/main/scala/Clover/Kafka/Producer.scala
+++ src/main/scala/Clover/Kafka/Producer.scala
@@ -1,12 +1,9 @@
 package Clover.Kafka
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.{IntegerSerializer, StringSerializer}
-import org.apache.spark.sql.{DataFrame, Encoders}
-import java.text.SimpleDateFormat
+import org.apache.spark.sql.DataFrame
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig}
 import org.apache.spark.sql.SparkSession
-import Clover.data.CRow
-
 import java.util.Properties
 class Producer(spark:SparkSession){
 
@@ -15,13 +12,6 @@ class Producer(spark:SparkSession){
    */
   def Produce(topicName:String = "team1"): Unit ={
 
-    val schema = Encoders.product[CRow].schema
-    /*val transactions2: Dataset[Row] = (spark.read.format("csv")
-      .option("delimiter", ",")
-      .option("header", "true")
-      .schema(schema)
-      .load("src/main/scala/Clover/data/files/BaseData/transactions.csv")
-      .as[Row])*/
     val transactions: DataFrame = spark.read.format("csv")
       .option("delimiter", ",")
       .option("header", "true")
@@ -33,8 +23,6 @@ class Producer(spark:SparkSession){
     prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[IntegerSerializer].getName)
     prop.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
 
-    val f =(x: String)=>{val format = new SimpleDateFormat("M/dd/yyy");format.parse(x).getTime}
-
     val prod = new KafkaProducer[Int,String](prop)
     var count = 1
     transactions.collect().foreach(row=>{
@@ -42,7 +30,6 @@ class Producer(spark:SparkSession){
       println(count)
       count +=1
     })
-
     prod.flush()
   }
 }
